498a921243955fa9a1ead7df3ba3530170212063
Luiz Kim <luiz.kim@controleonline.com>
2024-09-30 18:50:20 -0300

    Update

--- src/router/routes.js
+++ src/router/routes.js
@@ -14,5 +14,26 @@ export const routes = [
         component: () =>  import ('../pages/CRM/details.vue')
       }
     ]
-  }
+  },
+  {
+    path: "/proposal/models/",
+    component: () =>
+      import("@controleonline/ui-layout/src/layouts/AdminLayout.vue"),
+    children: [
+      {
+        name: "model",
+        path: "",
+        component: () =>
+          import("@controleonline/ui-crm/src/pages/Model"),
+      },
+      {
+        name: "modelDetails",
+        path: "id/:id",
+        component: () =>
+          import(
+            "@controleonline/ui-crm/src/pages/Model/Details.vue"
+          ),
+      },
+    ],
+  },
 ];
