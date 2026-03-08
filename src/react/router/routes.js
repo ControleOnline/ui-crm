cad040b66f73199f114feadb7d8f90361bc6f666
Luiz Kim <luiz.kim@controleonline.com>
2026-03-07 22:21:51 -0300

    Routes

--- src/react/router/routes.js
+++ src/react/router/routes.js
@@ -1,6 +1,5 @@
 import Comissions from '@controleonline/ui-crm/src/react/pages/comissions';
 import CrmIndex from '@controleonline/ui-crm/src/react/pages/crm/index';
-import ContractsPage from '@controleonline/ui-contracts/src/react/pages/ContractsPage';
 import ProposalsPage from '../pages/proposals';
 import CrmConversation from '../pages/crm/conversation';
 import CRMSettings from '../pages/settings/CRMSettings';
@@ -37,19 +36,10 @@ const crmRoutes = [
     },
   },
   {
-    name: 'ContractsIndex',
-    component: ContractsPage,
-    options: {
-      showBottomToolBar: true,
-      headerShown: true,
-      headerBackVisible: true,
-      title: 'Contratos',
-    },
-  },
-  {
     name: 'ProposalsIndex',
     component: ProposalsPage,
     options: {
+      showCompanyFilter: true,
       showBottomToolBar: true,
       headerShown: true,
       headerBackVisible: true,
